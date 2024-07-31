d5bf969d178e9e326b3768f9da8bdcc650fbedb3
Srinivas <srinivas.gundali@accenture.com>
2024-07-31 16:06:10 +0800

    initial setup and tailwind integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import User from "./User";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="testClass">Welcome {formatName(user)}</div>
+      {console.log("helooo")}
+      {/** the below component render line wont be visible in react devtools */}
+      {User({ name: formatName(user) })}
+      {/** the below component render line will be visible in react devtools */}
+      <User name={formatName(user)} />
+      <User name={formatName(user)} />
+      <User></User>
     </div>
   );
 }
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
 
+const user = {
+  firstName: "Harper",
+  lastName: "Perez",
+};
 export default App;
---
+++ src/User.js
@@ -0,0 +1,6 @@
+const User = (props) => {
+  //console.log("🚀 ~ User ~ User:", props);
+  return <h1>Name: {props?.name}</h1>;
+};
+
+export default User;
